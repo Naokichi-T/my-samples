b66d0d49fa38add354b1fe427be7bd646ce8a5b9
naokichi <naokichi.tany@gmail.com>
2022-11-12 13:37:49 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+// https://vanilla-extract.style/documentation/integrations/next/
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withVanillaExtract(nextConfig);
+
+// https://fwywd.com/tech/next-bundle-analyzer
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({});
